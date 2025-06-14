4b7aa561d73b9f55854e9fa70c6b45e441800ee4
multiversuskoth <multiversuskoth@gmail.com>
2025-06-13 19:50:54 -0500

    Perkspages

---
+++ src/database/PerkPages.ts
@@ -0,0 +1,32 @@
+import { prop, modelOptions, getModelForClass, Ref } from '@typegoose/typegoose';
+import { PlayerTester } from './PlayerTester';
+
+export class PerkSet {
+  @prop({ required: true })
+  public DisplayName!: string;
+
+  @prop({ required: true })
+  public Description!: string;
+
+  @prop({ type: () => [String], required: true })
+  public Perks!: string[];
+}
+
+@modelOptions({ schemaOptions: { _id: false } })
+export class CharacterPerkPages {
+  // Dynamic keys: "0", "1", ...
+  [setKey: string]: PerkSet | any;
+}
+
+@modelOptions({ schemaOptions: { _id: false } })
+export class PerkPages {
+  @prop({ required: true, ref: () => PlayerTester })
+  public player_id!: Ref<PlayerTester>;
+
+  @prop({ type: () => Object, required: true })
+  public perk_pages!: {
+    [character: string]: CharacterPerkPages;
+  };
+}
+
+export const PerkPagesModel = getModelForClass(PerkPages);
